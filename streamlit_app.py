5d726fad8d2418797150f4491ee793e44cf35295
Ultor31 <139874344+Ultor31@users.noreply.github.com>
2023-12-23 22:25:28 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,15 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+# New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+# This normalises the API response so that it is legible
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# This displays the normalised dataframe
+streamlit.dataframe(fruityvice_normalized)
